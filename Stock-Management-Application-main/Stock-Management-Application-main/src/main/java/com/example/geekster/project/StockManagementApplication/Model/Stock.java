19ec297367376c386f582ea793032184844ba8b5
Atul Kumar Upadhyay <137402498+Upadhyay123a@users.noreply.github.com>
2025-07-09 23:42:16 +0530

    Update Stock.java

--- Stock-Management-Application-main/Stock-Management-Application-main/src/main/java/com/example/geekster/project/StockManagementApplication/Model/Stock.java
+++ Stock-Management-Application-main/Stock-Management-Application-main/src/main/java/com/example/geekster/project/StockManagementApplication/Model/Stock.java
@@ -1,6 +1,6 @@
 package com.example.geekster.project.StockManagementApplication.Model;
 
-import jakarta.persistence.*;      
+import jakarta.persistence.*;       
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
